b2dd3b15cc45e3d00a72be12be9a24bc000e9175
tatsuya.shirai <tatsuya.shirai@nts.ricoh.co.jp>
2014-07-31 12:46:23 +0900

    add javascript-mode indent width

--- emacs.d/dev.el
+++ emacs.d/dev.el
@@ -80,8 +80,8 @@
 
 ;;JavaScript
 ; (add-to-list 'auto-mode-alist '("\\.js\\'" . javascript-mode))
-; (autoload 'javascript-mode "javascript" nil t)
-; (setq js-indent-level 2)
+(autoload 'javascript-mode "javascript" nil t)
+(setq js-indent-level 2)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (autoload 'js2-mode "js2-mode" nil t)
 ; (setq-default c-basic-offset 2)
