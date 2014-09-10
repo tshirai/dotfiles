45ff8c865ebe07eb926a201faae939f84edb84a2
shirai <tatsuya.py@gmail.com>
2014-09-10 14:18:22 +0900

    add java, phpenv and knife aliases

---
+++ bin/install_phpenv.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+
+# curl https://raw.github.com/CHH/phpenv/master/bin/phpenv-install.sh | bash
+curl https://raw.githubusercontent.com/CHH/phpenv/master/bin/phpenv-install.sh | bash
+git clone git://github.com/CHH/php-build.git ~/.phpenv/plugins/php-build
+
