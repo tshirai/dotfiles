0d459ee117a15cf5e281c56945e8b1816e67b46f
shirai <tatsuya.py@gmail.com>
2016-05-03 18:44:15 +0900

    add yum. rename npm install script

--- bin/install_cask.sh
+++ bin/install_cask.sh
@@ -1,10 +1,13 @@
-#! /bin/bash
+#! /bin/zsh
 # curl -fsSL https://raw.githubusercontent.com/cask/cask/master/go | python
 
 set -ex
 
 cd ~
-git clone https://github.com/cask/cask.git .cask
+if [ ! -d .cask ]; then
+  git clone https://github.com/cask/cask.git .cask
+fi
+source ~/.zshrc
 cd ~/.emacs.d
 if [ "${http_proxy}" = "" ]; then
     cask
---
+++ bin/yum_install.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+sudo yum install -y openssh zsh git
+sudo yum install -y telnet wget bind-utils nc tcpdump ngrep traceroute
+sudo yum install -y patchutils gcc gcc-c++ automake autoconf zlib zlib-devel
+sudo yum install -y readline readline-devel openssl openssl-devel
+sudo yum install -y kernel-headers glibc-headers rpm-build libtool doxygen
+sudo yum install -y lv man tree
+sudo yum install -y tmux subversion 
