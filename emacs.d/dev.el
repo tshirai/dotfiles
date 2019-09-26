723496b6b0b4d9ff8a89277f12567e90d36626f8
shirai <tatsuya.shirai@jp.ricoh.com>
2019-09-26 11:57:00 +0900

    add adoc-mode

--- emacs.d/dev.el
+++ emacs.d/dev.el
@@ -341,3 +341,9 @@
 
 ;; terraform
 ;; (add-hook 'terraform-mode-hook #'terraform-format-on-save-mode)
+
+;; AsciiDoc
+(autoload 'adoc-mode "adoc-mode" nil t)
+(require 'adoc-mode)
+; (add-to-list 'auto-mode-alist (cons "\\.adoc\\'" 'adoc-mode))
+; (add-hook 'adoc-mode-hook (lambda() (buffer-face-mode t)))
