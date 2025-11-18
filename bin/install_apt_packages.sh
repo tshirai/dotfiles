eceede821519c4232027c64b7cd8673e88c333bf
Tatsuya Shirai <tatsuya.shirai@jp.ricoh.com>
2025-11-18 09:54:29 +0900

    update

--- bin/install_apt_packages.sh
+++ bin/install_apt_packages.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+set -ex
 
 sudo apt -y install apt-show-versions lv tree curl
 sudo apt -y install language-pack-ja-base language-pack-ja ibus-mozc
@@ -6,3 +7,4 @@ sudo apt -y install build-essential libbz2-dev libdb-dev
 sudo apt -y install libreadline-dev libffi-dev libgdbm-dev liblzma-dev
 sudo apt -y install libncursesw5-dev libsqlite3-dev libssl-dev
 sudo apt -y install zlib1g-dev uuid-dev tk-dev libgnutls28-dev
+sudo apt -y install socat
--- bin/install_tmux.sh
+++ bin/install_tmux.sh
@@ -9,11 +9,11 @@ elif [ -f /etc/debian_version ]; then
 fi
 
 cd ~/local/src
-[ -f tmux-3.1.tar.gz ] || wget https://github.com/tmux/tmux/releases/download/3.1/tmux-3.1.tar.gz
-[ -d tmux-3.1 ] || tar zxf tmux-3.1.tar.gz
-cd tmux-3.1
-./configure --prefix=${HOME}/local/tmux-3.1
+[ -f tmux-3.2.tar.gz ] || wget https://github.com/tmux/tmux/releases/download/3.2/tmux-3.2.tar.gz
+[ -d tmux-3.2 ] || tar zxf tmux-3.2.tar.gz
+cd tmux-3.2
+./configure --prefix=${HOME}/local/tmux-3.2
 make
 make install
 cd ~/local
-ln -s ${HOME}/local/tmux-3.1 tmux
+ln -s ${HOME}/local/tmux-3.2 tmux
